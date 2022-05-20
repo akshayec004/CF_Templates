726a2c808e627e8cab6cd7a82ec8afa8fb08ac3b
akshayec004 <38517921+akshayec004@users.noreply.github.com>
2022-05-20 15:43:08 +0530

    Create aws-cf-s3.sh

---
+++ scripts/aws-cf-s3.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "Creating S3 Bucket via CF Stack"
+STACK_ID = $(aws cloudformation create-stack --stack-name s3bucket --template-body file://S3/S3_Create_Bucket.yaml --region 'ap-south-1')
+while true; do
+  echo "Checking stack creation, please wait."
+  STACK_STATUS="$(aws cloudformation describe-stacks --query 'Stacks[?StackId==`'"$STACK_ID"'`].StackStatus')"
+  if [ "$STACK_STATUS" == "CREATE_COMPLETE" ] || [ "$STACK_STATUS" == "UPDATE_COMPLETE" ]; then
+    echo "Stack Complete"
+	  exit 0
+  elif [ "$STACK_STATUS" == *"ROLL"* ]; then
+	  echo "Stack Rolled back"
+    exit 1
+	elif [ "$STACK_STATUS" == *"DELETE"* ]; then
+    echo "Stack Deleted"
+	  exit 1    
+  fi
+	sleep 10
+done
+  
